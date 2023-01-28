179140dd2f4b5db743a8beea32c8a914fe64ad0c
coscristian <crisquesadaco@gmail.com>
2023-01-28 16:33:52 -0500

    MVP added

---
+++ app/src/main/java/com/example/rickandmortyapp/Interface/InterfacesMainActivity.java
@@ -0,0 +1,16 @@
+package com.example.rickandmortyapp.Interface;
+
+public interface InterfacesMainActivity {
+    interface View {
+
+    }
+
+    interface Presenter {
+
+    }
+
+    interface Model {
+
+    }
+
+}
---
+++ app/src/main/java/com/example/rickandmortyapp/Model/Character.java
@@ -0,0 +1,5 @@
+package com.example.rickandmortyapp.Model;
+
+public class Character {
+
+}
